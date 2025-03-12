53cb8e63235915166754c3dc60fa870f705d4682
wangxingkang <wang_xingkang@qq.com>
2025-03-12 20:29:17 +0800

    docs: 添加示例图表目录

---
+++ docs/demos/demo/Pie/basic.tsx
@@ -0,0 +1,50 @@
+import type { PieProps } from '@sensoro-design/chart';
+import { Pie } from '@sensoro-design/chart';
+
+function Example() {
+  const spec: PieProps = {
+    height: 36,
+    width: 36,
+    padding: 0,
+    data: [
+      {
+        values: [
+          { type: 'oxygen', value: 46 },
+          { type: 'silicon', value: 27 },
+        ],
+      },
+    ],
+    outerRadius: 1,
+    innerRadius: 0.6,
+    padAngle: 2,
+    valueField: 'value',
+    categoryField: 'type',
+    legends: {
+      visible: false,
+    },
+    tooltip: {
+      mark: {
+        shapeType: 'square',
+        title: {
+          visible: false,
+        },
+        content: [
+          {
+            key: (datum) => {
+              return `${datum?.type}`;
+            },
+            value: (datum) => {
+              return `${datum?.value} 次`;
+            },
+          },
+        ],
+      },
+    },
+  };
+
+  return (
+    <Pie {...spec} />
+  );
+};
+
+export default Example;
