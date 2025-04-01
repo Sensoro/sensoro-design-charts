b639cc760aa0f7ec8a8623ef7c82b906b5efb136
wangxingkang <wang_xingkang@qq.com>
2025-04-01 14:29:27 +0800

    docs: 添加环图示例

---
+++ docs/demos/config/demo/Pie/ring.tsx
@@ -0,0 +1,74 @@
+import type { PieProps } from '@sensoro-design/chart';
+import { Pie } from '@sensoro-design/chart';
+import { darkColors } from '@sensoro-design/chart-theme';
+
+const list = [
+  { type: '正面', value: 24253 },
+  { type: '中性', value: 2602 },
+  { type: '负面', value: 274 },
+];
+
+function Example() {
+  const spec: PieProps = {
+    height: 400,
+    data: [
+      {
+        values: list,
+      },
+    ],
+    color: [darkColors.blue2, darkColors.green2, darkColors.red2],
+    outerRadius: 0.8,
+    innerRadius: 0.6,
+    valueField: 'value',
+    categoryField: 'type',
+    legends: {
+      item: {
+        shape: {
+          style: {
+            symbolType: 'rectRound',
+          },
+        },
+        label: {
+          widthRatio: 0.5,
+          formatMethod: (text, item) => {
+            // eslint-disable-next-line no-console
+            console.log(text, item);
+            return '测试';
+          },
+        },
+        value: {
+          formatter: '{label:~s}',
+        },
+      },
+    },
+    indicator: {
+      visible: true,
+      title: {
+        visible: true,
+        space: 12,
+        style: {
+          fontSize: 20,
+          fill: '#F6F9FE',
+          fontWeight: 600,
+          text: list.reduce<number>((prev, cur) => prev + cur.value, 0),
+        },
+      },
+      content: [
+        {
+          visible: true,
+          style: {
+            fontSize: 11,
+            fill: '#8194B5',
+            text: '总量',
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <Pie {...spec} />
+  );
+};
+
+export default Example;
