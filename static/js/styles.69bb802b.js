e5027915aa72c698db0935cefd095c8c900b2524
wangxingkang <wangxingkang@users.noreply.github.com>
2025-04-01 06:57:58 +0000

    deploy: 08c6f501ddb9b5b2e75ce7bedf0d40096437e1b9

--- static/js/styles.69bb802b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksensoro_design_charts=self.webpackChunksensoro_design_charts||[]).push([["212"],{1131:function(){},2798:function(){},6732:function(){},6991:function(){},6308:function(){},3672:function(){}}]);
