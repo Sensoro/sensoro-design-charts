2b0320e3186cf4b95e7b938471df3f693e0c6b5a
wangxingkang <wang_xingkang@qq.com>
2025-04-01 15:57:08 +0800

    docs: 优化词云图示例

--- docs/charts/demo/WordCloud/basic.tsx
+++ docs/charts/demo/WordCloud/basic.tsx
@@ -1,11 +1,8 @@
 import type { WordCloudProps } from '@sensoro-design/chart';
 import { WordCloud } from '@sensoro-design/chart';
-import React from 'react';
 import data from './data-wordcloud.json';
 
 function Example() {
-  const chartRef = React.useRef(null);
-
   const spec: WordCloudProps = {
     height: 400,
     nameField: 'challenge_name',
@@ -17,13 +14,8 @@ function Example() {
     },
   };
 
-  const handleClick: WordCloudProps['onClick'] = (e) => {
-    // eslint-disable-next-line no-console
-    console.log(e);
-  };
-
   return (
-    <WordCloud {...spec} ref={chartRef} onClick={handleClick} />
+    <WordCloud {...spec} />
   );
 }
 
