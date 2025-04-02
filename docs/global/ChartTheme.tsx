f7c33019e58e23fba89f4ae55b0852fdd9aaa745
wangxingkang <wang_xingkang@qq.com>
2025-04-02 20:26:05 +0800

    docs: 解决信息提示 shape 偏下

--- docs/global/ChartTheme.tsx
+++ docs/global/ChartTheme.tsx
@@ -1,13 +1,13 @@
 import { VChartCore } from '@sensoro-design/chart';
 import { dark, light } from '@sensoro-design/chart-theme';
-
 import React from 'react';
 import { useDark } from 'rspress/runtime';
+import './globale.css';
 
 const { ThemeManager } = VChartCore;
 
-ThemeManager.registerTheme('sensoroDesignLight', light);
-ThemeManager.registerTheme('sensoroDesignDark', dark);
+ThemeManager.registerTheme('sensoroDesignLight', light as any);
+ThemeManager.registerTheme('sensoroDesignDark', dark as any);
 
 function ChartTheme() {
   const dark = useDark();
