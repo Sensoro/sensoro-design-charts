61f05f007fd45e6546a19c11880ccd5a405e07ed
wangxingkang <wang_xingkang@qq.com>
2025-06-11 15:34:35 +0800

    docs: 示例优化

--- docs/custom/demo/TrendArea/_data.ts
+++ docs/custom/demo/TrendArea/_data.ts
@@ -1,3 +1,30 @@
+export const data = [
+  { date: 0, value: 5855 },
+  { date: 1, value: 4114 },
+  { date: 2, value: 3687 },
+  { date: 3, value: 3170 },
+  { date: 4, value: 2188 },
+  { date: 5, value: 2376 },
+  { date: 6, value: 9151 },
+  { date: 7, value: 44498 },
+  { date: 8, value: 54363 },
+  { date: 9, value: 56162 },
+  { date: 10, value: 79124 },
+  { date: 11, value: 77276 },
+  { date: 12, value: 96278 },
+  { date: 13, value: 99344 },
+  { date: 14, value: 80707 },
+  { date: 15, value: 82922 },
+  { date: 16, value: 83838 },
+  { date: 17, value: 70549 },
+  { date: 18, value: 65520 },
+  { date: 19, value: 65382 },
+  { date: 20, value: 44962 },
+  { date: 21, value: 29611 },
+  { date: 22, value: 12338 },
+  { date: 23, value: 6522 },
+];
+
 export const data1 = [
   { date: 0, value: 0 },
   { date: 1, value: 0 },
@@ -24,3 +51,5 @@ export const data1 = [
   { date: 22, value: 0 },
   { date: 23, value: 0 },
 ];
+
+export default {};
--- docs/custom/demo/TrendArea/field.tsx
+++ docs/custom/demo/TrendArea/field.tsx
@@ -1,4 +1,5 @@
 import { TrendArea } from '@sensoro-design/chart';
+import { data } from './_data';
 
 function Example() {
   return (
@@ -12,32 +13,7 @@ function Example() {
       selectTime={[3, 19]}
       xField="hour"
       yField="val"
-      data={[
-        { hour: 0, val: 5855 },
-        { hour: 1, val: 4114 },
-        { hour: 2, val: 3687 },
-        { hour: 3, val: 3170 },
-        { hour: 4, val: 2188 },
-        { hour: 5, val: 2376 },
-        { hour: 6, val: 9151 },
-        { hour: 7, val: 44498 },
-        { hour: 8, val: 54363 },
-        { hour: 9, val: 56162 },
-        { hour: 10, val: 79124 },
-        { hour: 11, val: 77276 },
-        { hour: 12, val: 96278 },
-        { hour: 13, val: 99344 },
-        { hour: 14, val: 80707 },
-        { hour: 15, val: 82922 },
-        { hour: 16, val: 83838 },
-        { hour: 17, val: 70549 },
-        { hour: 18, val: 65520 },
-        { hour: 19, val: 65382 },
-        { hour: 20, val: 44962 },
-        { hour: 21, val: 29611 },
-        { hour: 22, val: 12338 },
-        { hour: 23, val: 6522 },
-      ]}
+      data={data}
       tooltip={{
         dimension: {
           content: {
